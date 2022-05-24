5ca2b0750b008a1c7599e67363f04c92a03b8e9e
CDHZAYN <949135640@qq.com>
2022-05-24 17:48:55 +0800

    test5

--- src/test/java/com/tencent/wxcloudrun/dao/CommonDaoTest.java
+++ src/test/java/com/tencent/wxcloudrun/dao/CommonDaoTest.java
@@ -3,17 +3,19 @@ package com.tencent.wxcloudrun.dao;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+//@ContextConfiguration(locations= "classpath:application.yml")
 class CommonDaoTest {
     @Autowired
     CommonDao commonDao;
 
-    @Test
-    void getOpenIdTest() {
-        assertNull(commonDao.login("oriVB48y-JNhIqLsdf8W1adz7j0o"));
-        System.out.println(commonDao.login("oriVB48y-JNhIqLsdf8W1adz7j0o"));
-    }
+//    @Test
+//    void getOpenIdTest() {
+//        assertNull(commonDao.login("oriVB48y-JNhIqLsdf8W1adz7j0o"));
+//        System.out.println(commonDao.login("oriVB48y-JNhIqLsdf8W1adz7j0o"));
+//    }
 }
--- src/test/java/com/tencent/wxcloudrun/dao/SeekerDaoTest.java
+++ src/test/java/com/tencent/wxcloudrun/dao/SeekerDaoTest.java
@@ -3,10 +3,12 @@ package com.tencent.wxcloudrun.dao;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ContextConfiguration(locations= "classpath*:application.yml")
 class SeekerDaoTest {
 
 }
