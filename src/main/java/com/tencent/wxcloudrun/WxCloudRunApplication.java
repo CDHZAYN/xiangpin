fef68f9ba99849380ca500e6f247e47744e6d9a1
嘿，牛顿那个苹果是我掉的 <1173198173@qq.com>
2022-06-12 21:46:22 +0800

    fix: 服务端时区问题

--- src/main/java/com/tencent/wxcloudrun/WxCloudRunApplication.java
+++ src/main/java/com/tencent/wxcloudrun/WxCloudRunApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 import javax.sql.DataSource;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class WxCloudRunApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
         SpringApplication.run(WxCloudRunApplication.class, args);
     }
 }
