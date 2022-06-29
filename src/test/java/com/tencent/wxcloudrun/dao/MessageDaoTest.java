32dfe0d6770d9d2fe97db5a97b44c161a9e8705e
CDHZAYN <949135640@qq.com>
2022-06-30 01:08:57 +0800

    fix: reconstruct fix done

--- src/test/java/com/tencent/wxcloudrun/dao/MessageDaoTest.java
+++ src/test/java/com/tencent/wxcloudrun/dao/MessageDaoTest.java
@@ -18,28 +18,28 @@ class MessageDaoTest {
     @Autowired
     MessageDao dao;
 
-    @Test
-    void test() {
-        long time = 1655016890;
-        Date temp = new Date(1655016866 * 1000L);
-        System.out.println(temp);
-        System.out.println(dao.selectMessageByAccepterIDAndTime("124", temp));
-    }
-
-    @Test
-    void test2() {
-        String time = "1655021831895";
-        Date temp = new Date(Long.parseLong(time));
-        System.out.println(temp);
-        dao.updateMessageValue("123", "125", temp);
-    }
-
-    @Test
-    void test3() {
-        String time = "1655042677000";
-        Date temp = new Date(Long.parseLong(time));
-        System.out.println(temp);
-        MessagePO messagePO = new MessagePO(0, 0, 0, "123", "125", "离谱谱", temp);
-        dao.addMessage(messagePO);
-    }
+//    @Test
+//    void test() {
+//        long time = 1655016890;
+//        Date temp = new Date(1655016866 * 1000L);
+//        System.out.println(temp);
+//        System.out.println(dao.selectMessageByAccepterIDAndTime("124", temp));
+//    }
+//
+//    @Test
+//    void test2() {
+//        String time = "1655021831895";
+//        Date temp = new Date(Long.parseLong(time));
+//        System.out.println(temp);
+//        dao.updateMessageValue("123", "125", temp);
+//    }
+//
+//    @Test
+//    void test3() {
+//        String time = "1655042677000";
+//        Date temp = new Date(Long.parseLong(time));
+//        System.out.println(temp);
+//        MessagePO messagePO = new MessagePO(0, 0, 0, "123", "125", "离谱谱", temp);
+//        dao.addMessage(messagePO);
+//    }
 }
